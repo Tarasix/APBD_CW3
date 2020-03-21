d48d764c725992e0e157fa15abb6d6787623ff35
Taras Kostiuk <taraskostyuk5@gmail.com>
2020-03-21 16:52:50 +0100

    Done

---
+++ Cwicz_3/Controllers/StudentsController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cwicz_3.DAL;
+using Cwicz_3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cwicz_3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        private IDbService _dbService;
+
+        public StudentsController(IDbService service)
+        {
+            _dbService = service;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetStudents(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            }
+            else if (id == 2)
+            {
+                return Ok("Malewski");
+            }
+
+            return NotFound("Nie znaleziono studenta");
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult UsunStudenta(int id)
+        {
+            return Ok("Usuwanie ukonczone");
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdStudent(int id)
+        {
+            return Ok("Aktualizacja dokonczona");
+        }
+
+    }
+}
---
+++ Cwicz_3/DAL/IDbService.cs
@@ -0,0 +1,20 @@
+﻿using Cwicz_3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cwicz_3.DAL
+{
+    public interface IDbService
+    {
+        public IEnumerable <Student> GetStudents();
+    }
+
+
+
+
+
+
+
+}
